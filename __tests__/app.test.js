71e76fc38487a6944592b1cfed3814d79b74ad3c
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-03-29 15:32:32 -0700

    wrote sign in user test, controller, model. test is passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 
 describe('Top-secret-backend routes', () => {
   beforeEach(() => {
@@ -15,9 +16,21 @@ describe('Top-secret-backend routes', () => {
   it('creates a user', async () => {
     const res = await request(app)
       .post('/api/v1/auth/signup')
-      .send({ username: 'Wally West', password: 'souvenir', });
+      .send({ username: 'Wally', password: 'souvenir', });
 
-    expect(res.body).toEqual({ id: expect.any(String), username: 'Wally West' });
+    expect(res.body).toEqual({ id: expect.any(String), username: 'Wally' });
+  });
+
+  it('signs in a user', async () => {
+    const user = await UserService.create({
+      username: 'Wally',
+      password: 'souvenir'
+    });
+    const res = await request(app)
+      .post('/api/v1/auth/signin')
+      .send({ username: 'Wally', password: 'souvenir' });
+
+    expect(res.body).toEqual({ user });
   });
 
 });
